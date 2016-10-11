08e8ba592f1b0981f305bc388ed5ffd7e3a7019d
Alex Chojnacki <thealex@umich.edu>
2016-10-11 15:59:52 -0400

    necessary files

---
+++ p2.cpp
@@ -0,0 +1,69 @@
+#include <cassert>
+#include "p2.h"
+
+int sum(list_t list) {
+    assert(false);
+}
+int product(list_t list) {
+    assert(false);
+}
+int accumulate(list_t list, int (*fn)(int, int), int identity){
+    assert(false);
+}
+
+list_t reverse(list_t list){
+     assert(false);
+}
+
+list_t append(list_t first, list_t second){
+     assert(false);
+}
+
+list_t filter_odd(list_t list){
+     assert(false);
+}
+
+list_t filter_even(list_t list){
+     assert(false);
+}
+
+list_t filter(list_t list, bool (*fn)(int)){
+     assert(false);
+}
+
+list_t rotate(list_t list, int n){
+     assert(false);
+}
+
+list_t insert_list(list_t first, list_t second, int n){
+     assert(false);
+}
+
+list_t chop(list_t list, int n){
+     assert(false);
+}
+
+int fib(int n){
+     assert(false);
+}
+
+int fib_tail(int n){
+     assert(false);
+}
+
+int tree_sum(tree_t tree){
+     assert(false);
+}
+
+list_t traversal(tree_t tree){
+     assert(false);
+}
+
+bool contained_by(tree_t A, tree_t B){
+     assert(false);
+}
+
+tree_t insert_tree(int elt, tree_t tree){
+     assert(false);
+}
+
